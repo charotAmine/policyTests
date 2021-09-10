260e48e7148ae7b29461abed52c0d9af6c257ce5
charotAmine <42579419+charotAmine@users.noreply.github.com>
2021-09-10 12:12:31 +0000

    Add logs

--- tests/policy.tests.ps1
+++ tests/policy.tests.ps1
@@ -1,10 +1,11 @@
 describe 'Deploy a public storage' {
     it 'Not be deployed' {
+        write-output "Starting test : "
         terraform -chdir="$($PSScriptRoot)/terraformFiles/" apply
         terraform init
         terraform plan
         $output = terraform apply -auto-approve 
-        
+        write-output "MY OUTPUT : $output"
         $policyName = "articlePost_PA"
         "$output" -like "*$policyName*" | Should -Be $true
     }
