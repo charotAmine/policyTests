b6824403e31d1b30bd8aa55fc846860639045abe
charotAmine <42579419+charotAmine@users.noreply.github.com>
2021-09-10 08:47:20 +0000

    Fix policy

--- policy.tf
+++ policy.tf
@@ -3,7 +3,10 @@ locals {
  
   policy_files_without_extension = [ for p in local.policy_files: split(".", p)[0] ]
  
-  policy_json = jsondecode(file(join("", [var.policy_folder, policy, ".json"])))
+  policy_json = {
+    for policy in local.policy_files_without_extension:
+        policy => jsondecode(file(join("", [var.policy_folder, policy, ".json"])))
+  }
 }
 
 resource "azurerm_policy_definition" "policy" {
